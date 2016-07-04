341c3f01b262533821f44feaf6d39ea885a5d09b
= <jameswright1981@ymail.com>
2016-07-04 16:29:07 +0100

    Updated-about.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,11 @@ var About = React.createClass({
   render: function () {
     return (
         <div>
-            <h3>About</h3>
-            <p>This is a simple app for getting the temperature of a specified city</p>
+            <h1 className="text-center">About</h1>
+            <p>This is a simple app for getting the temperature of a specified city.</p>
+            <p>It was built using React.js and the temperature information is
+                supplied by <a href="http://OpenWeathermap.org">OpenWeathermap.org</a>.</p>
+            <p>You can find out more about React <a href="https://facebook.github.io/react/">here</a>.</p>
         </div>
     )
   }
